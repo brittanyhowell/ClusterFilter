003c92ce28331eb3c7ceefbd7d36664d239d17e7
brittanyhowell <brittany.howell@student.adelaide.edu.au>
2016-08-13 15:20:35 +0930

    Changed to accept multiple clusters for sequential alignments.

--- muscle.sh
+++ muscle.sh
@@ -2,7 +2,7 @@
 ## Script invokes Muscle
 ## Date: 12-8-2016
 
-# Invoked by: 
+# Invoked by: WkDIR=/data/rc003/Brittany/Data/Clusters sbatch muscle.sh
 
 #SBATCH -p batch
 #SBATCH -N 1 
@@ -19,7 +19,21 @@
 # Load the necessary modules
 module load MUSCLE/3.8.31
 
+ClusDIR=/data/rc003/Brittany/Clusters/clusters
+MuscleOUT=/data/rc003/Brittany/Clusters/MuscleAlign
 
-muscle -in cluster_7.fasta -out cluster_7.afa
+CLUSTERS=("0" "1" "2" "3" "6" "7" "8" "10" "25" "108" "115" "146" "150" "151" "198" "425")
+
+# Move into cluster directory
+cd ${WkDIR}
+
+for clusterNum in "${CLUSTERS[@]}"; do 
+
+echo "aligning cluster ${clusterNum}"
+
+	muscle -in cluster_${clusterNum}.fasta -out ${MuscleOUT}/cluster_7.afa
+
+done
 
 echo "Complete"
+
