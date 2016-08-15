33d6a5a3906dc28a79c05aa79c28b66689aaa0c5
brittanyhowell <brittany.howell@student.adelaide.edu.au>
2016-08-15 11:39:23 +0930

    new script Script makes trees using FastTree, using many muscle aligned clusters.

---
+++ makeTrees.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# makeTrees.sh
+# 
+
+AlignDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/ClusterFilter/clusterOutput/AlignedClusters
+TreeDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/ClusterFilter/clusterOutput/tree
+
+# Check TreeDIR exists 
+if [ -d $TreeDIR ]; then
+	rm -r $TreeDIR 
+	mkdir $TreeDIR
+	echo "TREE folder exists... replacing"  
+else 
+	echo "creating TREE folder"  
+	mkdir $TreeDIR
+fi
+
+cd ${AlignDIR}
+
+# Remove any semi-complete coverage files
+if [ -f *.tree ]; then
+	rm *.tree
+	echo "Removed tree file in progress"  
+else 
+	echo "No tree files in progress"  
+fi 
+
+echo "making trees" 
+for iAlign in *form.afa; do 
+filename=${i%.form.afa}
+	fasttree -nt ${iAlign} > ${filename}.tree
+	mv ${filename}.tree ${TreeDIR}
+done
+
+echo "Complete"
