7529b029959ff997778612f7f1f7c9417b7d08bb
brittanyhowell <brittany.howell@student.adelaide.edu.au>
2016-08-16 11:51:57 +0930

    Added file replacement and table creation Summary files now replace themselves if they existed. Table is generated with information on how many clusters exist and how many have more than 20 sequences.

--- getClusterData.sh
+++ getClusterData.sh
@@ -2,25 +2,67 @@
 
 # File paths 
 splitDir=/Users/brittanyhowell/Documents/University/Honours_2016/Project/ClusterFilter/clusterOutput/splitting
-resultsDir=/Users/brittanyhowell/Documents/University/Honours_2016/Project/ClusterFilter/clusterOutput/splitting/OutputInformation
+clusDIR=${splitDir}/clusters
+resultsDir=${splitDir}/OutputInformation
 
 # Run information
-resultsTable=results_0_.9.uc
-cluster="0"
-id=".9"
+cluster="3"
+id=".95"
 minClustSize="20"
+resultsTable=results_${cluster}_${id}.uc
+
+# Output files:
+AllClust="clusterList_${cluster}_${id}.txt"
+Min20="clusterList_min${minClustSize}_${cluster}_${id}.txt"
+SummTable="clusterNumbers.txt"
 
 
 # Clustering
 cd ${splitDir}
 
-usearch -cluster_fast cluster_${cluster}.fasta -id ${id} -clusters ${splitDir}/cluster_${cluster}_${id}_ -uc ${resultsTable}
+if [ -f ${resultsTable} ]; then
+	rm ${resultsTable}
+	echo "Removed ${resultsTable}... Making"  
+else 
+	echo "Making ${resultsTable}"  
+fi 
+
+usearch -cluster_fast cluster_${cluster}.fasta -id ${id} -clusters ${clusDIR}/cluster_${cluster}_${id}_ -uc ${resultsTable}
+
+mv ${resultsTable} ${resultsDir}
 
 # Edit results table into cluster summary tables
 cd ${resultsDir}
 
-awk '{if ($1=="C") print}' ${resultsTable} > clusterList_${cluster}_${id}.txt
+if [ -f ${AllClust} ]; then
+	rm ${AllClust}
+	echo "Removed ${AllClust}... Making"  
+else 
+	echo "Making ${AllClust}"  
+fi 
 
-awk '{if ($3>='${minClustSize}') print}' clusterList_${cluster}_${id}.txt > clusterList_min20_${cluster}_${id}.txt
+# find clusters
+awk '{if ($1=="C") print}' ${resultsTable} > ${AllClust}
+
+if [ -f ${Min20} ]; then
+	rm ${Min20}
+	echo "Removed ${Min20}... Making"  
+else 
+	echo "Making ${Min20}"  
+fi 
+
+# Find clusters with more than min members
+awk '{if ($3>='${minClustSize}') print}' clusterList_${cluster}_${id}.txt > ${Min20}
+
+
+# Add line to Summary table
+
+NumClusters=$(wc -l ${AllClust})
+NumLargeClusters=$(wc -l ${Min20})
+
+echo -e  "${NumClusters} \t ${NumLargeClusters}" >> ${SummTable}
 
 echo "Complete"
+
+
+
