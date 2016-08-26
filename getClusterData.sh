7cd482932b11b91d7747832f42b8169d42f4a100
brittanyhowell <brittany.howell@student.adelaide.edu.au>
2016-08-26 14:21:25 +0930

    Changed for one single cluster Removed "cluster" variable Changed input names

--- getClusterData.sh
+++ getClusterData.sh
@@ -1,26 +1,48 @@
 #!/bin/bash
 
 # File paths 
-splitDir=/Users/brittanyhowell/Documents/University/Honours_2016/Project/ClusterFilter/clusterOutput/splitting
-clusDIR=${splitDir}/clusters
-resultsDir=${splitDir}/OutputInformation
+seqDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/ClusterFilter/ClusFromActive
+clusDIR=${seqDIR}/clusters
+resultsDir=${seqDIR}/OutputInformation
 
 # Run information
-cluster="0"
+#cluster="0"
+InputSeq="mouseORF1ORF2min4kbmax8kb.fasta"
 id=".95"
 idName="95"
 minClustSize="20"
-resultsTable=results_${cluster}_${id}.uc
+resultsTable=results_BO_4-8_${id}.uc
 
 # Output files:
-ClustSeqs="${clusDIR}/cluster_${cluster}_${idName}_"
-AllClust="clusterList_${cluster}_${id}.txt"
-Min20="clusterList_min${minClustSize}_${cluster}_${id}.txt"
-SummTable="clusterNumbersSecond.txt"
+ClustSeqs="${clusDIR}/cluster_${idName}_"
+AllClust="clusterList_${id}.txt"
+Min20="clusterList_min${minClustSize}_${id}.txt"
+SummTable="clustersBO_4-8.txt"
 
+# Folder checks
+if [ -d $seqDIR ]; then
+	echo "Sequence folder exists..." 
+else 
+	echo "No Sequence folder.... exiting" 
+	exit
+fi 
+
+if [ -d $clusDIR ]; then
+	echo "cluster output folder exists..."
+else 
+	echo "No cluster output folder.... Creating" 
+	mkdir ${clusDIR}
+fi 
+
+if [ -d $resultsDir ]; then
+	echo "Results folder exists..."
+else 
+	echo "No Results folder.... Creating" 
+	mkdir ${resultsDir}
+fi 
 
 # Clustering
-cd ${splitDir}
+cd ${seqDIR}
 
 if [ -f ${resultsTable} ]; then
 	rm ${resultsTable}
@@ -29,7 +51,7 @@ else
 	echo "Making ${resultsTable}"  
 fi 
 
-usearch -cluster_fast cluster_${cluster}.fasta -id ${id} -clusters ${ClustSeqs} -uc ${resultsTable}
+usearch -cluster_fast ${InputSeq} -id ${id} -clusters ${ClustSeqs} -uc ${resultsTable}
 
 
 mv ${resultsTable} ${resultsDir}
@@ -55,7 +77,7 @@ else
 fi 
 
 # Find clusters with more than min members
-awk '{if ($3>='${minClustSize}') print}' clusterList_${cluster}_${id}.txt > ${Min20}
+awk '{if ($3>='${minClustSize}') print}' clusterList_${id}.txt > ${Min20}
 
 
 # Add line to Summary table
