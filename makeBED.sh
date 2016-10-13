fedd324399695baebb360bd4704c4ca66b1528b5
brittanyhowell <brittany.howell@student.adelaide.edu.au>
2016-10-13 20:38:30 +1030

    changed for aligned sequence data added some seds to use the headers already edited by renameHeader.sh

--- makeBED.sh
+++ makeBED.sh
@@ -1,19 +1,22 @@
+
 #!/bin/bash
 ## Uses data in headers to make BED files
+## Update on 13th October to read from alignments with renameHeader edited headers.
 ## Date: 19-8-2016
 
-largeClusDir=/Users/brittanyhowell/Documents/University/Honours_2016/Project/ClusterFilter/ClusFromActive/compile
-WkDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/ClusterFilter/ClusFromActive/clusters
-CLUSTERS=("0" "1" "2" "3" "4" "5" "6" "7" "8" "10" "16")
+largeClusDir=/Users/brittanyhowell/Documents/University/Honours_2016/Project/ClusterFilter/splitting/AlignedClusters/sequences/BED
+WkDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/ClusterFilter/splitting/AlignedClusters/sequences
+# CLUSTERS=("0" "1" "2" "3" "4" "5" "6" "7" "8" "10" "16")
 
 # Move into working directory
 cd ${WkDIR}
 
-for clusterNum in "${CLUSTERS[@]}"; do  
+for clusterNum in *.fasta; do  
 
 echo "making BED from cluster ${clusterNum}"
+filename=${clusterNum%.fasta}
 
-	cat cluster_95_${clusterNum}.fasta | grep '>' | sed 's/>//g' | sed 's/:/ /g' | sed 's/(-)/.rev/g' | sed 's/(+)/.fwd/g' | sed 's/-/ /g' | sed 's/.rev/ -/g' | sed 's/.fwd/ +/g' | awk '{print $1 "\t" $2 "\t" $3 "\t" "L1mouse" "\t" "1" "\t" $4}' > ${largeClusDir}/cluster_95_${clusterNum}.bed
+	cat ${clusterNum} | grep '>' | sed 's/ORF1//g' | sed 's/ORF2//g' | sed 's/>//g' | sed 's/\_/ /g' | sed 's/ -/.rev/g' | sed 's/ +/.fwd/g' | sed 's/-/ /g' | sed 's/.rev/ -/g' | sed 's/.fwd/ +/g' | awk '{print $1 "\t" $2 "\t" $3 "\t" "L1mouse" "\t" "1" "\t" $4}' > ${largeClusDir}/${filename}.bed
 
 done
 
@@ -22,4 +25,5 @@ cd ${largeClusDir}
 cat cluster* > ActiveClusteredL1s.bed 
 
 echo "Complete"
+
 
